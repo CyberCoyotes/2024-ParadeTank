f98fc63abd9e99d3b2da300ae4323907494d1435
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2024-09-11 16:20:56 -0400

    testing a new subsystem2 and command2

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -7,24 +7,28 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class DriveSubsystem extends SubsystemBase {
+    /**
+     * The DifferentialDrive class represents a drivetrain with two motors, allowing for differential steering.
+     * It provides methods for controlling the drivetrain's movement and rotation.
+     */
     private final DifferentialDrive drive;
     private final WPI_TalonSRX rightLeaderDrive;
-    // private final WPI_TalonSRX rightFollowerDrive;
+    private final WPI_TalonSRX rightFollowerDrive;
     private final WPI_TalonSRX leftLeaderDrive;
-    // private final WPI_TalonSRX leftFollowerDrive;
+    private final WPI_TalonSRX leftFollowerDrive;
     private final WPI_TalonSRX[] motors;
 
     public DriveSubsystem() {
         rightLeaderDrive = new WPI_TalonSRX(Constants.RIGHT_LEADER_ID);
-        // rightFollowerDrive = new WPI_TalonSRX(Constants.RIGHT_FOLLOWER_ID);
+        rightFollowerDrive = new WPI_TalonSRX(Constants.RIGHT_FOLLOWER_ID);
         leftLeaderDrive = new WPI_TalonSRX(Constants.LEFT_LEADER_ID);
-        // leftFollowerDrive = new WPI_TalonSRX(Constants.LEFT_FOLLOWER_ID);
+        leftFollowerDrive = new WPI_TalonSRX(Constants.LEFT_FOLLOWER_ID);
 
         motors = new WPI_TalonSRX[]{
             rightLeaderDrive, 
-            // rightFollowerDrive,
-            leftLeaderDrive
-            //,leftFollowerDrive
+            rightFollowerDrive,
+            leftLeaderDrive,
+            leftFollowerDrive
         };
 
         configureMotors();
@@ -33,9 +37,9 @@ public class DriveSubsystem extends SubsystemBase {
 
     private void configureMotors() {
         rightLeaderDrive.setInverted(true);
-        // rightFollowerDrive.setInverted(true);
+        rightFollowerDrive.setInverted(false);
         leftLeaderDrive.setInverted(false);
-        // leftFollowerDrive.setInverted(false);
+        leftFollowerDrive.setInverted(true);
 
         for (WPI_TalonSRX motor : motors) {
             motor.configFactoryDefault();
