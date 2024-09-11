714cffdff64ded8c8337bf385c0bb44a76ea7e60
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2024-09-11 15:31:51 -0400

    Update DriveSubsystem.java

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -9,18 +9,23 @@ import frc.robot.Constants;
 public class DriveSubsystem extends SubsystemBase {
     private final DifferentialDrive drive;
     private final WPI_TalonSRX rightLeaderDrive;
-    private final WPI_TalonSRX rightFollowerDrive;
+    // private final WPI_TalonSRX rightFollowerDrive;
     private final WPI_TalonSRX leftLeaderDrive;
-    private final WPI_TalonSRX leftFollowerDrive;
+    // private final WPI_TalonSRX leftFollowerDrive;
     private final WPI_TalonSRX[] motors;
 
     public DriveSubsystem() {
         rightLeaderDrive = new WPI_TalonSRX(Constants.RIGHT_LEADER_ID);
-        rightFollowerDrive = new WPI_TalonSRX(Constants.RIGHT_FOLLOWER_ID);
+        // rightFollowerDrive = new WPI_TalonSRX(Constants.RIGHT_FOLLOWER_ID);
         leftLeaderDrive = new WPI_TalonSRX(Constants.LEFT_LEADER_ID);
-        leftFollowerDrive = new WPI_TalonSRX(Constants.LEFT_FOLLOWER_ID);
+        // leftFollowerDrive = new WPI_TalonSRX(Constants.LEFT_FOLLOWER_ID);
 
-        motors = new WPI_TalonSRX[]{rightLeaderDrive, rightFollowerDrive, leftLeaderDrive, leftFollowerDrive};
+        motors = new WPI_TalonSRX[]{
+            rightLeaderDrive, 
+            // rightFollowerDrive,
+            leftLeaderDrive
+            //,leftFollowerDrive
+        };
 
         configureMotors();
         drive = new DifferentialDrive(rightLeaderDrive, leftLeaderDrive);
@@ -28,16 +33,16 @@ public class DriveSubsystem extends SubsystemBase {
 
     private void configureMotors() {
         rightLeaderDrive.setInverted(true);
-        rightFollowerDrive.setInverted(true);
+        // rightFollowerDrive.setInverted(true);
         leftLeaderDrive.setInverted(false);
-        leftFollowerDrive.setInverted(false);
+        // leftFollowerDrive.setInverted(false);
 
         for (WPI_TalonSRX motor : motors) {
             motor.configFactoryDefault();
         }
 
-        rightFollowerDrive.set(TalonSRXControlMode.Follower, rightLeaderDrive.getDeviceID());
-        leftFollowerDrive.set(TalonSRXControlMode.Follower, leftLeaderDrive.getDeviceID());
+        // rightFollowerDrive.set(TalonSRXControlMode.Follower, rightLeaderDrive.getDeviceID());
+        // leftFollowerDrive.set(TalonSRXControlMode.Follower, leftLeaderDrive.getDeviceID());
     }
 
     public void drive(double drivePower, double turnPower) {
